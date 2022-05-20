10651f3f925121b064190bbeca0ec88649615c95
Nick <82569587+IceTeaAlchemist@users.noreply.github.com>
2022-05-20 16:47:07 -0500

    Numerous events added. Check description.

--- caspar.js
+++ caspar.js
@@ -2,14 +2,27 @@ const engine = require('./build/Release/casparengine');
 const WebSocket = require('ws');
 const event = require('events');
 const { Server } = require('http');
+const { clearInterval } = require('timers');
 
+
+console.log("Welcome to CASPAR--C-based Analysis of Samples by PCR with Adaptive Routine.");
+console.log("----------Runtime Status----------");
+
+console.log("Server running.");
 const wss = new WebSocket.Server({ port: 7071});
 
 let DataIntervId;
+let PCRIntervId;
 
 var isRunning = false;
 
 wss.on('connection', function connection(ws) {
+    var connect = {
+        id: "connect",
+        status: "Server connected."
+    };
+    ws.send(JSON.stringify(connect));
+    console.log("Connected to client.");
     ws.on('message', function message(data){
         var msg = JSON.parse(data);
         switch(msg.id){
@@ -19,43 +32,75 @@ wss.on('connection', function connection(ws) {
                     engine.start(function (){});
                     isRunning = true;
                     console.log('Ignition started.');
-                    DataIntervId = setInterval(function sendit(){ws.send(engine.readdata());}, 100);
+                    DataIntervId = setInterval(sendit, 300);
+                    PCRIntervId = setInterval(sendPCR, 5000);
                 }
                 else
                 {
+                    clearInterval(DataIntervId);
+                    clearInterval(PCRIntervId);
                     engine.stop();
                     isRunning = false;
                     console.log('Cycling shutdown.');
-                    clearInterval(DataIntervId);
+                }
+                break;
+            case "filename":
+                console.log(msg.newname);
+                if(isRunning === false)
+                {
+                    engine.changefiles(msg.newname);
+                    ws.send(JSON.stringify({id: "filestatus", status: "File changed."}));
+                }
+                else
+                {
+                    ws.send(JSON.stringify({id: "filestatus", status: "Currently running PCR. Filename rejected."}));
                 }
                 break;
             case "shutdown": 
-            engine.stop();
-            isRunning = false;
-            console.log("Server disconnected.");
-            process.exit();
+                if(isRunning === true)
+                {
+                    engine.stop();
+                }
+                isRunning = false;
+                console.log("Server disconnected.");
+                ws.send(JSON.stringify({id: "connect", status: "Server disconnected."}));
+                process.exit();
         }
         console.log('received: %s', data);
         console.log(isRunning);
     });
-    ws.send('something');
 })
+
+function sendit()
+{
+    var datastruct = {
+        id: "cycledata",
+        fluor: engine.readdata(),
+        temp: 0 // engine.readtemp()
+    };
+    wss.clients.forEach(function dataupdate(ws) {ws.send(JSON.stringify(datastruct));});
+    
+}
+
+function sendPCR()
+{
+    var PCRinfo = engine.readPCR();
+    if(PCRinfo[0] > 0)
+    {
+        console.log("Sending PCR.");
+        var datastruct = {
+            id: "PCRdata",
+            fam: PCRinfo[0],
+            cyfive: PCRinfo[1]
+        };
+        wss.clients.forEach(function pcrupdate(ws) {ws.send(JSON.stringify(datastruct));});
+    }
+    else
+    {
+        //do nothing
+    }
+}
 
 
 
 engine.initializePCR();
-
-//engine.start();
-
-//setInterval(readout, 500);
-
-/*function readout()
-{
-    wss.send(JSON.stringify(engine.readdata()));
-}*/
-
-console.log(engine.hello());
-
-console.log(engine.readdata())
-
-console.log(engine.sum_up())
