ed3defe0ac2fa71c093d42d2b3ab886661c62cba
Nick <82569587+IceTeaAlchemist@users.noreply.github.com>
2022-05-20 16:45:34 -0500

    Numerous function additions--check description
    reopenFiles() added to change filename.  ReadPCR() added to output PCR values to JS interface.  startrun now resets cycle count on run.

--- src/casparapi.cpp
+++ src/casparapi.cpp
@@ -27,6 +27,12 @@ namespace demo
 
         void Execute()
         {
+            cycles = 0;
+            //openFiles();
+            clearactivedata();
+            recordflag = true;
+            setupADC();
+            calibrategain();
             runflag = true;
             cycle();
         }
@@ -50,8 +56,6 @@ namespace demo
         setupPi();
         setupQiagen();
         openFiles();
-        setupADC();
-        calibrategain();
     }
 
     void Iteration(const FunctionCallbackInfo<Value>& args)
@@ -69,13 +73,31 @@ namespace demo
     {
         Isolate* isolate = args.GetIsolate();
         double jsoutput;
-        if(runflag == true)
+        if(runflag == true && recordflag == true)
         {
             jsoutput = y[y.size()-1];
         }
         else
         {
-            jsoutput = -1;
+            jsoutput = 0;
+        }
+        Local<Number> num = Number::New(isolate, jsoutput);
+        args.GetReturnValue().Set(num);
+    }
+
+    void readoutTemp(const FunctionCallbackInfo<Value>& args)
+    {
+        Isolate* isolate = args.GetIsolate();
+        double jsoutput;
+        if(runflag == true && recordflag == true)
+        {
+            float voltage = (TEMP.getreading()*4.096)/32767.0;
+            float temperature = (voltage - 1.25)/0.005;
+            jsoutput = temperature;
+        }
+        else
+        {
+            jsoutput = 0;
         }
         Local<Number> num = Number::New(isolate, jsoutput);
         args.GetReturnValue().Set(num);
@@ -83,20 +105,63 @@ namespace demo
 
     void readoutPCR(const FunctionCallbackInfo<Value>& args)
     {
-        // This is another placeholder. readPCR needs to be updated.
+        Isolate* isolate = args.GetIsolate();
+        v8::Local<v8::Array> pcrhandoff;
+        if(pcrReady == true)
+        {
+            pcrhandoff = New<v8::Array>(pcrValues.size());
+            for(int i = 0; i < pcrValues.size(); i++)
+            {
+                Nan::Set(pcrhandoff, i, New<v8::Number>(pcrValues[i]));
+                //pcrhandoff->Set(i, Number::New(isolate,pcrValues[i]));
+            }
+            pcrValues.clear();
+            pcrReady = false;
+        }
+        else
+        {
+            pcrhandoff = New<v8::Array>(pcrValues.size());
+            for(int i = 0; i < pcrValues.size(); i++)
+            {
+                Nan::Set(pcrhandoff, i, New<v8::Number>(-1));
+                //pcrhandoff->Set(i, Number::New(isolate,pcrValues[i]));
+            }
+        }
+        args.GetReturnValue().Set(pcrhandoff);
     }
 
 
 
     void startRun(const FunctionCallbackInfo<Value>& args)
     {
-        runflag = true;
-        cycle();
+        
     }
 
     void stopRun(const FunctionCallbackInfo<Value>& args)
     {
         runflag = false;
+        recordflag = false;
+        //D2.resetI2C();
+        //closeFiles();
+        sens1.LED_off(1);
+        sens1.LED_off(2);
+        sens2.LED_off(1);
+        sens2.LED_off(2);
+    }
+
+    void reopenFiles(const FunctionCallbackInfo<Value>& args)
+    {
+        Isolate* isolate = args.GetIsolate();
+        String::Utf8Value str(isolate, args[0]);
+
+        string newfilename(*str);
+        string ts = timestamp();
+
+        coeffstorage = "./data/" + newfilename+ "coeff" + ts + ".txt";
+        pcrstorage = "./data/" + newfilename + "pcr" + ts + ".txt";
+        rawstorage = "./data/" + newfilename + "binaryoutput" + ts + ".bin";
+        closeFiles();
+        openFiles();
     }
 
     void checkInit(const FunctionCallbackInfo<Value>& args)
@@ -119,6 +184,9 @@ namespace demo
         //NODE_SET_METHOD(exports, "start", startRun);
         NODE_SET_METHOD(exports,"stop", stopRun);
         NODE_SET_METHOD(exports, "readdata", readoutData);
+        NODE_SET_METHOD(exports, "readtemp", readoutTemp);
+        NODE_SET_METHOD(exports, "readPCR", readoutPCR);
+        NODE_SET_METHOD(exports, "changefiles", reopenFiles);
     }
 
     NODE_MODULE(casparengine, Initialize)
